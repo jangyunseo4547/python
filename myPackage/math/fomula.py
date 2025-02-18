6f482d807b063792447f3a986329f240c88a3c2a
yunseo <jysjys2669@gmail.com>
2025-02-18 14:32:41 +0900

    update

---
+++ myPackage/math/fomula.py
@@ -0,0 +1,14 @@
+pi = 3.14
+
+def my_max(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return factorial(n-1) * n==
